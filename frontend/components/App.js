809df965a52bf6ba34b22235d431b182e7e711f1
Nana Kumi <baider11222gmail.com>
2022-05-10 22:37:46 -0400

    project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,69 @@
 import React from 'react'
+import axios from 'axios'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
+  state = {
+    todos:[],
+    error:'',
+    todoNameInput:''
+  }
+  onTodoNameInputChange=evt=>{
+    const{value} = evt.target 
+    this.setState({...this.state, todoNameInput: value})
+  }
+  resetForm = () => this.setState({...this.state, todoNameInput:''})
+  setAxiosResponseError = err => this.setState({...this.state, error: err.response.data.message})
+  postNewTodo = () => {
+    axios.post(URL,{name: this.state.todoNameInput})
+    .then(res => {
+      this.setState({...this.state, todos: this.state.todos.concat(res.data.data)})
+      this.resetForm()
+    })
+    .catch(this.setAxiosResponseError)
+  }
+  onTodoFormSubmit = (evt) =>{
+    evt.preventDefault()
+    this.postNewTodo()
+
+  }
+  fetchAllTodos = ()=>{
+    axios.get(URL)
+    .then(res=>{
+      this.setState({...this.state,todos: res.data.data})
+    })
+    .catch(this.setAxiosResponseError)
+  }
+  toggleCompleted = id => evt =>{
+    axios.patch(`${URL}/${id}`)
+    .then(res =>{
+      debugger
+    })
+     
+  }
+  componentDidMount(){
+    this.fetchAllTodos()
+
+  }
   render() {
-    return null
+    return (
+      <div>
+        <div id ="error"> Error:{this.state.error}</div>
+        <div id='todos'>
+          <h2>Todos:</h2>
+          {
+            this.state.todos.map(td =>{
+              return <div onClick={this.toggleCompleted(td.id)}key={td.id}>{td.name}{td.completed ? '✅':''}</div>
+            })
+          }
+        </div>
+        <form id ='todoForm' onSubmit={this.onTodoFormSubmit}>
+          <input value={this.state.todoNameInput} onChange={this.onTodoNameInputChange}type = 'text' placeholder= 'Type todo'></input>
+          <input type='submit'></input>
+          <button>Clear Completed</button>
+        </form>
+      </div>
+    )
   }
 }
